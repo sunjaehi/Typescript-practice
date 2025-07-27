dbdfec3f94238db9a69fbd5bc43dc1989566420c
Lee Sunjae <sunjae7944@naver.com>
2025-07-27 23:17:00 +0900

    강의코드 - Dispatch

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useReducer, act } from 'react';
+import React, { useState, useRef, useEffect, useReducer, act, useContext } from 'react';
 import './App.css';
 import Editor from './component/Editor';
 import { Todo } from './types';
@@ -24,6 +24,18 @@ function reducer(state : Todo[], action:Action) {
       return state.filter((it) => it.id !== action.id);
     }
   }
+}
+
+export const TodoStateContext = React.createContext<Todo[] | null>(null);
+export const TodoDispatchContext = React.createContext<{
+  onClickAdd : (text: string) => void;
+  onClickDelete : (id : number) => void;
+} | null>(null);
+
+export function useTodoDispatch() {
+  const dispatch = useContext(TodoDispatchContext);
+  if(!dispatch) throw new Error("TodoDispatch에 문제가 있다");
+  return dispatch;
 }
 
 function App() {
@@ -55,12 +67,23 @@ function App() {
   return (
     <div className="App">
       <h1>Todo</h1>
-      <Editor onClickAdd={onClickAdd}></Editor>
-      <div>
-        {todos.map((todo)=> (
-          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete}/>
-        ))}
-      </div>
+      <TodoStateContext.Provider value={todos}>
+        <TodoDispatchContext.Provider
+          value={{
+            onClickAdd,
+            onClickDelete,
+          }}
+        >
+          <Editor></Editor>
+          <div>
+            {todos.map((todo)=> (
+              <TodoItem key={todo.id} {...todo}/>
+            ))}
+          </div>
+        </TodoDispatchContext.Provider>
+      
+      </TodoStateContext.Provider>
+      
     </div>
   );
 }
--- section11/src/component/Editor.tsx
+++ section11/src/component/Editor.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { TodoDispatchContext, useTodoDispatch } from "../App";
 
 interface Props {
-    onClickAdd : (text : string) => void;
+    
 }
 
 export default function Editor(props : Props) {
 
+    const dispatch = useTodoDispatch();
     const [text, setText] = useState("");
 
     const onChangeInput = (e : React.ChangeEvent<HTMLInputElement>) => {
@@ -13,9 +15,9 @@ export default function Editor(props : Props) {
     }
 
     const onClickButton = () => {
-        props.onClickAdd(text);
+        dispatch.onClickAdd(text);
         setText("");
-    }
+    };
 
     return (
     <div>
